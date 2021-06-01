dd557676ec30d90af8c601e4c17bdbb3d046944f
Julien Hofer <hoferj@uni-hildesheim.de>
2021-06-01 17:48:34 +0200

    Added store index

--- src/store/index.js
+++ src/store/index.js
@@ -6,18 +6,7 @@ Vue.config.devtools = true
 
 export default new Vuex.Store({
     modules: {
-       items,
        concepts,
-       project,
        concept_map,
-       members,
-       daily_scrum,
-       reflexion,
-       documentation,
-       tool,
-       todo,
-       assistent,
-       phases,
-       output_documents
     }
 });
