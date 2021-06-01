23ca152e7190cd57d2e277352a4d84c90be4eb75
Julien Hofer <hoferj@uni-hildesheim.de>
2021-06-01 17:48:16 +0200

    Deleted scaffold from vue create

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ConceptMap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConceptMap from './components/ConceptMap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ConceptMap
   }
 }
 </script>
